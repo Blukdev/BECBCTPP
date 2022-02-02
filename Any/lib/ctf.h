a7a10ddc940e6d62c75bd99626925a3fdc35261b
KCN-judu <79190965+KCN-judu@users.noreply.github.com>
2022-02-02 15:25:25 +0800

    CTF

--- Any/lib/ctf.h
+++ Any/lib/ctf.h
@@ -6,15 +6,15 @@
 
 using namespace std;
 int CmdCounter;
-int TurnToward, ChainToward, TurnHistory, Initialize;
+int TurnToward = 2, ChainToward = 1, TurnHistory = -2, Initialize = 0;
 
 struct CommandBlock {
     int id = -1;
-    string command = "";
+    int command;
 };
 
 struct s {
-    int x, y, z;
+    int x = 0, y = 0, z = 0;
 };
 
 CommandBlock BlockInfo[64][64][64];
@@ -31,75 +31,74 @@ inline void CTFlip() {
     else ChainToward = 1;
 }
 
-pair <vector<CommandBlock>, s> CTBPairing(int types, vector <string> &commands) {
+pair <vector<CommandBlock>, s> CTBPairing(int CmdCounter) {
     s size, Counter, pos;
     int LinearCounter = 1;
-    TurnToward = 2;
-    TurnHistory = -2;
-    ChainToward = 1;
-    Initialize = 0;
-    Counter.x = Counter.y = Counter.z = pos.x = pos.y = pos.z = 0;
-    CmdCounter = types;
-
-    for (int i = 1; CmdCounter > 0;) {
+    while (CmdCounter > 0) {
         Counter.z++;
         if (Initialize == 0) {
             CmdCounter--;
             BlockInfo[pos.x][pos.y][pos.z].id = Initialize;
-            BlockInfo[pos.x][pos.y][pos.z].command = commands.at(0);
+            BlockInfo[pos.x][pos.y][pos.z].command = 0;
             Initialize = -2;
             pos.z++;
             continue;
         }
-        //绗竴涓懡浠ゆ柟鍧楀垵濮嬪寲
+
         if (pos.z != 0 && pos.z != 63) {
             BlockInfo[pos.x][pos.y][pos.z].id = ChainToward;
-            BlockInfo[pos.x][pos.y][pos.z].command = commands.at(LinearCounter);
+            BlockInfo[pos.x][pos.y][pos.z].command = LinearCounter;
             LinearCounter++;
-            if (ChainToward == 1)pos.z++;
-            else pos.z--;
+            pos.z = (ChainToward == 1) ? pos.z + 1 : pos.z - 1;
             CmdCounter--;
         }
 
         else if ((pos.z == 0 && pos.x == 63 && pos.y % 2 == 0) || (pos.z == 63 && pos.x == 0 && pos.y % 2 == 1)) {
             TTFlip();
             BlockInfo[pos.x][pos.y][pos.z].id = TurnToward;
+            BlockInfo[pos.x][pos.y][pos.z].command = -1;
             pos.y++;
             CTFlip();
             BlockInfo[pos.x][pos.y][pos.z].id = ChainToward;
-            if (ChainToward == 1)pos.z++;
-            else pos.z--;
+            BlockInfo[pos.x][pos.y][pos.z].command = -1;
+            pos.z = (ChainToward == 1) ? pos.z + 1 : pos.z - 1;
             TTFlip();
             Counter.y++;
         }
 
         else {
             BlockInfo[pos.x][pos.y][pos.z].id = TurnToward;
+            BlockInfo[pos.x][pos.y][pos.z].command = -1;
             if (TurnToward == 2)pos.x++;
             else pos.x--;
             CTFlip();
             BlockInfo[pos.x][pos.y][pos.z].id = ChainToward;
-            if (ChainToward == 1)pos.z++;
-            else pos.z--;
+            BlockInfo[pos.x][pos.y][pos.z].command = -1;
+            pos.z = (ChainToward == 1) ? pos.z + 1 : pos.z - 1;
             Counter.x++;
         }
     }
+
     if (Counter.y != 0) {
         size.x = size.z = 64;
         size.y = Counter.y + 1;
-    } else if (Counter.x != 0) {
+    }
+
+    else if (Counter.x != 0) {
         size.y = 1;
         size.x = Counter.x + 1;
         size.z = 64;
-    } else {
+    }
+
+    else {
         size.y = size.x = 1;
         size.z = Counter.z;
     }
 
     vector <CommandBlock> result;
 
-    for (int y = 0; y < size.y; y++)
-        for (int x = 0; x < size.x; x++)
+    for (int x = 0; x < size.x; x++)
+        for (int y = 0; y < size.y; y++)
             for (int z = 0; z < size.z; z++)
                 result.push_back(BlockInfo[x][y][z]);
     return {result, size};
